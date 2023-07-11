99c2b51d81750427292af10c79dd70edb3e8222d
mojca <mojca.org@gmail.com>
2023-07-11 16:42:41 +0200

    Added wikipedia connectors + dependencies

--- wordcloud/src/main/java/com/outbrain/wordcloud/wiki/WikipediaController.java
+++ wordcloud/src/main/java/com/outbrain/wordcloud/wiki/WikipediaController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-    @RestController
+import java.util.List;
+
+@RestController
     @RequestMapping("api")
     public class WikipediaController {
 
@@ -16,14 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
         }
 
         @GetMapping("wiki") //http://localhost:8080/api/wiki?title=blabla
-        public WikipediaData articleEndpoint(@RequestParam String title) {
+        public WikipediaData wikiDataEndpoint(@RequestParam String title) {
 
             return wikipediaRepository.findByTitle(title).get(0);
         }
 
         @GetMapping("relations") //http://localhost:8080/api/wiki?title=blabla
-        public WikipediaRelations WikipediaRarticleEndpoint(@RequestParam String title) {
+        public WikipediaRelations wikiRelationsEndpoint(@RequestParam String title) {
 
             return wikipediaRepository.findByTitle(title).get(0).relations();
+        }
+
+        @GetMapping("wikitext") //http://localhost:8080/api/wiki?title=blabla
+        public List<String> wikiTextEndpoint(@RequestParam String title) {
+
+            return wikipediaRepository.findByTitle(title).get(0).parsedParagraphs();
         }
 }
