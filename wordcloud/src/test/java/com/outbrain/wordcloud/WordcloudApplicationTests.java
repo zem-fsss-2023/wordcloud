524722e9a3a0c63c44edd9dd52941aa5303553c8
jernejak12 <jerneja.krajcar@gmail.com>
2023-07-11 16:10:38 +0200

    sprint init

---
+++ wordcloud/src/test/java/com/outbrain/wordcloud/WordcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outbrain.wordcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
