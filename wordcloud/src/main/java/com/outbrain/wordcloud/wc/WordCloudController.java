853f732b0cdacf367e6020eb5dd5739351386e9d
mojca <mojca.org@gmail.com>
2023-07-12 14:09:17 +0200

    Added metrics (timer, counter)

--- wordcloud/src/main/java/com/outbrain/wordcloud/wc/WordCloudController.java
+++ wordcloud/src/main/java/com/outbrain/wordcloud/wc/WordCloudController.java
@@ -2,11 +2,15 @@ package com.outbrain.wordcloud.wc;
 
 import com.outbrain.wordcloud.wc.dto.WordCloudRequest;
 import com.outbrain.wordcloud.wiki.WikipediaRepository;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.Counter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("api")
@@ -15,17 +19,25 @@ public class WordCloudController {
     private WikipediaRepository wikipediaRepository;
     private WordCloudClient wordCloudClient;
     private final Logger logger = LoggerFactory.getLogger(WordCloudClient.class.getName());
+    private final Counter wordCloudCounter;
+    private final Timer wordCloudTimer;
 
-    public WordCloudController(WikipediaRepository wikipediaRepository, WordCloudClient wordCloudClient) {
+    public WordCloudController(WikipediaRepository wikipediaRepository, WordCloudClient wordCloudClient, final MeterRegistry meterRegistry) {
         this.wikipediaRepository = wikipediaRepository;
         this.wordCloudClient = wordCloudClient;
+        this.wordCloudTimer = meterRegistry.timer("WordCloudController timer");
+        this.wordCloudCounter = meterRegistry.counter("WordCloudController counter");
 
     }
 
     @GetMapping("wc") //http://localhost:8080/api/wc?text=blabla insert text here blabla
     public byte[] wordCloudTextEndpoint(@RequestParam String text) throws IOException {
+        long startTime = System.currentTimeMillis();
         logger.info("Called wordCloudTextEndpoint with text=" + text.substring(0, (Math.min(text.length(), 50))));
-        return wordCloudClient.getWordCloud(text).body().asInputStream().readAllBytes();
+        wordCloudCounter.increment();
+        byte[] response = wordCloudClient.getWordCloud(text).body().asInputStream().readAllBytes();
+        wordCloudTimer.record(System.currentTimeMillis()-startTime, TimeUnit.MILLISECONDS);
+        return response;
     }
 
     @PostMapping("wc") //http://localhost:8080/api/wiki
